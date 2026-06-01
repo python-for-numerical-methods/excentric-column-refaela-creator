29ff2431e7cb0c07bb63b147d23dd6b03e6674dc
refaela-creator <refaela@ac.sce.ac.il>
2026-06-01 23:15:02 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -1,3 +1,6 @@
+import math
+import scipy.optimize as opt
+
 def find_critical_load(L, E, A, r, c, e, sigma_allow):
     """
     L: אורך במ"מ
@@ -10,4 +13,26 @@ def find_critical_load(L, E, A, r, c, e, sigma_allow):
 
     Return: העומס P בניוטון (float)
     """
-    # כתבו כאן את הקוד
+    
+    # מומנט האינרציה (I = A * r^2)
+    I = A * (r ** 2)
+    
+    # חישוב עומס אוילר המשמש כחסם עליון לחיפוש שלנו
+    euler_limit = (math.pi ** 2 * E * I) / (L ** 2)
+    
+    # חישוב קבועים מראש כדי למנוע חישובים מיותרים בתוך הלולאה
+    eccentricity_ratio = (e * c) / (r ** 2)
+    
+    # הפונקציה שאותה אנחנו רוצים לאפס (הפרש המאמצים)
+    def equation_to_solve(current_p):
+        if current_p <= 0:
+            return -sigma_allow
+            
+        # הארגומנט של פונקציית הסקאנט
+        angle_rad = (L / (2 * r)) * math.sqrt(current_p / (E * A))
+        secant_val = 1.0 / math.cos(angle_rad)
+        
+        # חישוב המאמץ הנוכחי
+        current_stress = (current_p / A) * (1.0 + eccentricity_ratio * secant_val)
+        
+        # מר
